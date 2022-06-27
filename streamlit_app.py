2d14b3e7b59b0b57cb113e52e964ead63d9804b7
fdzlucas <64571815+fdzlucas@users.noreply.github.com>
2022-06-27 14:42:39 -0600

    Update streamlit_app.py
    added details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit 
 
 streamlit.title("New tracking system")
+
+streamlit.header("Weekend Schedule")
+streamlit.text("Saturday & Sunday morning run")
+streamlit.text("Saturday & Sunday morning gym")
+streamlit.text("Saturday & Sunday evening violin") 
